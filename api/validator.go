319c42347d319d24f537b042a496a730f3abf6c5
Sasha Levshin <ocarin31@yandex.ru>
2021-06-05 16:07:34 +0700

    fix validation errors

--- api/validator.go
+++ api/validator.go
@@ -5,12 +5,6 @@ import (
 	"github.com/go-playground/validator/v10"
 )
 
-var supportedCurrencies = map[string]bool{
-	"USD": true,
-	"EUR": true,
-	"CAD": true,
-}
-
 var validCurrency validator.Func = func(fieldLevel validator.FieldLevel) bool {
 	if currency, ok := fieldLevel.Field().Interface().(string); ok {
 		// check currency is supported
